be589c90111c65f8c01ae10532242ef17d354193
sieff osman <sieffosman@aol.com>
2023-08-05 12:59:24 +0100

    Nav Bar fully functional

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
-  const NavList = document.querySelector('.headerNavList');
-  const navLinkItems = Array.from(NavList.querySelectorAll('a'));
+  const navList = document.querySelector('.headerNavList');
+  const navLinks = navList.querySelectorAll('a');
 
   hamburger.addEventListener('click', () => {
-    NavList.classList.toggle('active');
+    navList.classList.toggle('active');
   });
 
-  navLinkItems.forEach((link) => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      NavList.classList.remove('active');
-
-      navLinkItems.forEach((item) => {
-        item.classList.remove('active');
-      });
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.forEach(link => link.classList.remove('active'));
       link.classList.add('active');
     });
   });
+
+  // Underline the link corresponding to the current page
+  const currentPage = window.location.pathname.split('/').pop();
+  const currentLink = Array.from(navLinks).find(link => link.href.endsWith(currentPage));
+  if (currentLink) {
+    currentLink.classList.add('active');
+  }
 });
